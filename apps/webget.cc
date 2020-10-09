46420a2586a233c84e829d765dce2757657d8e81
Zijun Yu <yu1319324245@gmail.com>
2020-10-09 19:55:16 +0800

    update gitignore and finish webget

--- apps/webget.cc
+++ apps/webget.cc
@@ -17,8 +17,19 @@ void get_URL(const string &host, const string &path) {
     // (not just one call to read() -- everything) until you reach
     // the "eof" (end of file).
 
-    cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
-    cerr << "Warning: get_URL() has not been implemented yet.\n";
+    // cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
+    // cerr << "Warning: get_URL() has not been implemented yet.\n";
+
+    TCPSocket sock1;
+    sock1.connect(Address(host, "http"));
+
+    sock1.write("GET " + path + " HTTP/1.1\r\n"
+                + "Host: " + host + "\r\n"
+                + "Connection: close\r\n\r\n");
+    while (!sock1.eof()) {
+        cout << sock1.read();
+    }
+    sock1.close();
 }
 
 int main(int argc, char *argv[]) {
