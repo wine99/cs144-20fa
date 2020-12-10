339ceb0451c1702cbe3fc3d08e9ea0c947096641
Zijun Yu <yu1319324245@gmail.com>
2020-12-10 13:06:37 +0800

    lab2: wrap and unwrap

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -14,8 +14,7 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    return WrappingInt32{static_cast<uint32_t>(n) + isn.raw_value()};
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +28,32 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    // understand the following code first
+
+    // uint32_t a = 1;
+    // uint32_t b = static_cast<uint32_t>((1UL << 32) - 1UL); // 4294967295
+    // uint32_t x = a - b;
+    // int32_t  y = a - b;
+    // 2 2
+    // uint32_t x = b - a;
+    // int32_t  y = b - a;
+    // 4294967294 -2
+
+    // uint32_t a = 1;
+    // uint32_t b = 0;
+    // uint32_t x = a - b;
+    // int32_t  y = a - b;
+    // 1 1
+    // uint32_t x = b - a;
+    // int32_t  y = b - a;
+    // 4294967295 -1
+
+    // STEP ranges from -UINT32_MAX/2 to UINT32_MAX/2
+    // in most cases, just adding STEP to CHECKPOINT will get the absolute seq
+    // but if after adding, the absolute seq is negative, it should add another (1UL << 32)
+    // (this means the checkpoint is near 0 so the new seq should always go bigger
+    // eg. test/unwrap.cc line 25)
+    int32_t steps = n - wrap(checkpoint, isn);
+    int64_t res = checkpoint + steps;
+    return res >= 0 ? checkpoint + steps : res + (1UL << 32);
 }
